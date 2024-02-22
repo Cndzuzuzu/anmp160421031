535a815ec6e98b30fe943e9d0500e8dba57d64a7
Cndzuzuzu <118964356+Cndzuzuzu@users.noreply.github.com>
2024-02-22 14:23:52 +0700

    sign up added

---
+++ app/src/main/java/com/zuzudev/anmp_160421031/SignUpActivity.kt
@@ -0,0 +1,11 @@
+package com.zuzudev.anmp_160421031
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}

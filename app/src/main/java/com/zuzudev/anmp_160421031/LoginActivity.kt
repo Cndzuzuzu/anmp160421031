c45299fe222fa790e9160e3921483e720f3b3b6a
TrueGre3n <oeyraymon4@gmail.com>
2024-02-22 14:36:40 +0700

    login comadn

--- app/src/main/java/com/zuzudev/anmp_160421031/LoginActivity.kt
+++ app/src/main/java/com/zuzudev/anmp_160421031/LoginActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        //login yey
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
     }

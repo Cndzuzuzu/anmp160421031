4b94ddc972b6367e1f62ad7e323b4e29b91e04b2
Cndzuzuzu <118964356+Cndzuzuzu@users.noreply.github.com>
2024-02-22 14:09:49 +0700

    login activity added

---
+++ app/src/main/java/com/zuzudev/anmp_160421031/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.zuzudev.anmp_160421031
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
